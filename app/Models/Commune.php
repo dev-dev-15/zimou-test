95917cb0434d751dc21e411b504fe3aa2e88f3b0
dev <0proboy0@gmail.com>
2024-11-09 02:27:22 -0800

    Add relationship method for Wilaya in Commune model

--- app/Models/Commune.php
+++ app/Models/Commune.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Commune extends Model
 {
     protected $fillable = ['name', 'wilaya_id'];
+
+    public function wilaya()
+    {
+        return $this->belongsTo(Wilaya::class);
+    }
 }
