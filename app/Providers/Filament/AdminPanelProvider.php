3277a515256c34f44f3fbef4b5a19aed40a40bfd
dev <0proboy0@gmail.com>
2024-11-09 07:22:52 -0800

    Enable users registration

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -28,6 +28,7 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
+            ->registration()
             ->brandLogo(asset('imgs/zimou-express-logo.png'))
             ->brandLogoHeight('2.5rem')
             ->favicon(asset('imgs/favicon.ico'))
