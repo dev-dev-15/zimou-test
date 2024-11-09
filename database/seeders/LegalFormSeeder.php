d302418dd70c85d08241efe82cf643ac0cea577b
dev <0proboy0@gmail.com>
2024-11-09 01:34:49 -0800

    Refactor LegalFormSeeder to use a class property for legal forms data

--- database/seeders/LegalFormSeeder.php
+++ database/seeders/LegalFormSeeder.php
@@ -7,22 +7,22 @@ use Illuminate\Support\Facades\DB;
 
 class LegalFormSeeder extends Seeder
 {
+    protected $legalForms = [
+        ['name' => 'Physical person (Individual Entrepreneur)'],
+        ['name' => 'One-person Limited Liability Company (EURL)'],
+        ['name' => 'Limited Liability Company (SARL)'],
+        ['name' => 'Joint Stock Company (SPA)'],
+        ['name' => 'General Partnership (SNC)'],
+        ['name' => 'Simple Limited Partnership (SCS)'],
+        ['name' => 'Partnership Limited by Shares (SCA)'],
+        ['name' => 'Group (GR)'],
+    ];
+
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        $legalForms = [
-            ['name' => 'Physical person (Individual Entrepreneur)'],
-            ['name' => 'One-person Limited Liability Company (EURL)'],
-            ['name' => 'Limited Liability Company (SARL)'],
-            ['name' => 'Joint Stock Company (SPA)'],
-            ['name' => 'General Partnership (SNC)'],
-            ['name' => 'Simple Limited Partnership (SCS)'],
-            ['name' => 'Partnership Limited by Shares (SCA)'],
-            ['name' => 'Group (GR)'],
-        ];
-
-        DB::table('legal_forms')->insert($legalForms);
+        DB::table('legal_forms')->insert($this->legalForms);
     }
 }
